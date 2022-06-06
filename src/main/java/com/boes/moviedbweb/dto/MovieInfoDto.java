cb978d5165b46d80cffda5292f0a50e0cc7d4e12
Greg Boes <gboes@pistolbarrel.net>
2022-06-06 11:46:07 -0500

    Added ability to parse correctly even if 'both' title lines are present in the input.

--- src/main/java/com/boes/moviedbweb/dto/MovieInfoDto.java
+++ src/main/java/com/boes/moviedbweb/dto/MovieInfoDto.java
@@ -20,28 +20,49 @@ public class MovieInfoDto {
     private boolean absolute;
 
     public static MovieDto convertToMovieDto(MovieInfoDto movieInfo) {
+        final int DURATION_INDEX = 2;
+        final int COLLECTIONS_INDEX = 3;
+        final int TITLE_INDEX = 5;
+        final int TITLE_EXTRA_INDEX = 6;
+        // the following can be influenced with parseAdjust
+        final int DIRECTORS_INDEX = 6;
+        final int COUNTRIES_INDEX = 7;
+        final int ACTORS_INDEX = 8;
+        final int DESCRIPTION_INDEX = 9;
+
         List<String> extractedStrings = List.of(movieInfo.info.split("\r?\n|\r"));
-        StringBuilder descriptionBuilder = new StringBuilder();
+        int parseAdjust = 0;
         if (extractedStrings.size() >= 12) {
-            for (int idx = 10; idx < extractedStrings.size() - 1; idx++) {
-                descriptionBuilder.append(extractedStrings.get(idx));
-                descriptionBuilder.append(System.getProperty("line.separator"));
+            /*
+             Looking for a couple of possibilities here.
+             1) The movie has the title and the title Watched lines entered
+             2) the description is multi line
+             The 1st will cause an index change for most of the parsing
+             */
+            if (extractedStrings.get(TITLE_EXTRA_INDEX).contains(extractedStrings.get(TITLE_INDEX))) {
+                parseAdjust = 1;
             }
         }
+        StringBuilder descriptionBuilder = new StringBuilder(extractedStrings.get(DESCRIPTION_INDEX + parseAdjust));
+        // the following will not trigger unless extractedStrings.size() >= 12
+        for (int idx = DESCRIPTION_INDEX + parseAdjust + 1; idx < extractedStrings.size() - 1; idx++) {
+            descriptionBuilder.append(System.getProperty("line.separator"));
+            descriptionBuilder.append(extractedStrings.get(idx));
+        }
 
-        Title title = new Title(extractedStrings.get(5));
-        String s = extractedStrings.get(7);
-        String fixedCountries = s.replace(",", ";");
+        Title title = new Title(extractedStrings.get(TITLE_INDEX));
         MovieDto dto = new MovieDto(
                 title.getName(),
                 replaceNoneWithEmpty(title.getYear()),
-                replaceNoneWithEmpty(descriptionBuilder.toString()),  // description
-                replaceNoneWithEmpty(extractedStrings.get(8)),    // actors
-                replaceNoneWithEmpty(extractedStrings.get(6)),    // directors
-                replaceNoneWithEmpty(fixedCountries),
-                extractedStrings.get(3),    // collections
-                LocalDate.now().toString(), // viewDate
-                extractedStrings.get(2),    // duration
+                replaceNoneWithEmpty(descriptionBuilder.toString()),
+                replaceNoneWithEmpty(extractedStrings.get(ACTORS_INDEX + parseAdjust)),
+                replaceNoneWithEmpty(extractedStrings.get(DIRECTORS_INDEX + parseAdjust)),
+                replaceNoneWithEmpty(
+                        extractedStrings.get(COUNTRIES_INDEX + parseAdjust).replace(",", ";")
+                ),
+                extractedStrings.get(COLLECTIONS_INDEX),
+                LocalDate.now().toString(),
+                extractedStrings.get(DURATION_INDEX),
                 movieInfo.isAbsolute()
         );
         if (!StringUtils.isBlank(movieInfo.viewDate)) {
